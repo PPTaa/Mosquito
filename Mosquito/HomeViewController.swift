338d15f0851e25fe431738dc13ee1135bf8d3259
pptaa <chobo05@naver.com>
2023-03-06 19:54:00 +0900

    v0.0.1 FEAT: GIT INIT

---
+++ Mosquito/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mosquito
+//
+//  Created by leejungchul on 2023/03/03.
+//
+
+import UIKit
+import Alamofire
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mosquito/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Mosquito
+//
+//  Created by leejungchul on 2023/03/03.
+//
+
+import Foundation
